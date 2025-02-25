d383ec8d42ae6fa225ba722609a65a72cfc3a1db
Hanna Angeria <haan2402@student.miun.se>
2025-02-25 15:56:41 +0100

    gjort diagram för kurserna

--- src/main.js
+++ src/main.js
@@ -21,3 +21,66 @@ function toggleMenu() {
         navEl.style.display = "none"
     }
 }
+
+/**
+ * Hämta statistik på antagning från JSON-filen och konverterar den
+ * @async
+ * @function getCourses hämtar data från API
+ * @throws {Error} Om det är problem med nätverksanslutning
+ */
+
+window.onload = () => {
+    getCourses();
+}
+
+async function getCourses() {
+    try {
+        const response = await fetch("https://studenter.miun.se/~mallar/dt211g/");
+        if(!response.ok) {
+            throw new Error('Problem med nätverksanslutning')
+        }
+        const data = await response.json();
+        console.log(data);
+        chartCourses(data);
+        chartPrograms(data);
+
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+/**
+ * Skapar stapeldiagram med de 6 mest sökta programmen
+ * @function
+ */
+function chartCourses(data) {
+    const popularCourses = data 
+    .filter(item => item.type === "Kurs")
+    .sort((a, b) => parseInt(b.applicantsTotal) - parseInt(a.applicantsTotal))
+    .slice(0, 6);
+
+    const courseName = popularCourses.map(course => course.name);
+    const numberOfApplicants = popularCourses.map(course => parseInt(course.applicantsTotal));
+   
+    const chartsBar = document.getElementById('bar-chart');
+
+    new Chart(chartsBar, {
+        type: 'bar',
+        data: {
+            labels: courseName,
+            datasets: [{
+                label: 'Antal sökande',
+                data: numberOfApplicants,
+                borderWidth: 1,
+                backgroundColor: ['Red','Blue','Yellow','Green','Orange','Purple']
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
