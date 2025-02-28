2965a6e88e4dc67258a7148496395a6264cd42b8
Hanna Angeria <haan2402@student.miun.se>
2025-02-28 15:15:29 +0100

    lagt in sökfunktion till kartan och hämtar info från API och uppdaterar plats på kartan

--- src/main.js
+++ src/main.js
@@ -137,6 +137,9 @@ function chartPrograms(data) {
 
 /**
  * Hämtar in data från Leafletjs API och lägger in karta
+ * @map - lagt till Kalix koordinater där start markeringen på kartan är
+ * @L - använt Leafletjs guide för att hämta in karta till min sida
+ * @marker - lagt till markering för Kalix och adderar till kartan
  */
 const map = L.map('map').setView([66.0, 23.0], 14);
 
@@ -146,4 +149,34 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const marker = L.marker([66.0, 23.0]).addTo(map);
+
+/**
+ * Gör funktion för att kunna söka efter plats och uppdatera kartan
+ * @function - hämtar först in value från mitt input fält där man söker efter stad, sedan hämtar jag från nominatim API, konverterar til json, där jag lägger in Searchresults för att få in den sökta platsen
+ * @fetch - för att hämta in data från json filen, och tar ut lattitud och longitud, använder setView och L.marker för att uppdatera plats
+ */
+function getCoordinates() {
+    const searchResult = document.getElementById('search').value;
+
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${(searchResult)}`;
+
+    fetch(url) 
+        .then(response => response.json())
+        .then(data => {
+            if(data.length > 0) {
+                const lat = data[0].lat;
+                const lon = data[0].lon;
+
+                map.setView([lat, lon], 14);
+                L.marker([lat, lon]).addTo(map);;
+            }
+        })
+        .catch(error => (error));
+}
+
+/**
+ * Eventlyssnare för att kunna använda sökknappen och få den nya platsen man sökt på
+ */
+document.getElementById('search-btn').addEventListener("click", getCoordinates);
+
 
